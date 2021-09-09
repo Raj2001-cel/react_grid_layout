7b2b7c41a3e3c1ce98ee08cb39b680201e3e1cf9
Raj Mourya <rajhmourya@gmail.com>
2021-09-09 23:31:15 +0530

    added a react grid layout for resuability

--- src/index.js
+++ src/index.js
@@ -1,17 +1,92 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React  from "react";
+import ReactDom from "react-dom";
+import './index.css'
+// function Greeting(){
+//     return <h4>this my first entry point</h4>
+// }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const BookList = [{
+
+    img:"https://images-eu.ssl-images-amazon.com/images/I/41HpU3PBslS._AC_SX184_.jpg",
+    title:'The Illuminated',
+    author:'Anindita Ghose',
+    price:'234Rs',
+    rating:120
+
+},{
+  img:"https://images-eu.ssl-images-amazon.com/images/I/51ztXlYS2kS._AC_SX184_.jpg",
+  title:'SONG OF DRAUPADISONG OF DRAUPADI',
+  author:'Ira Mukhoty',
+  price:'450Rs',
+  rating:53
+
+},{
+img:"https://images-eu.ssl-images-amazon.com/images/I/51KEpmJ8gYL._AC_SX184_.jpg",
+title:"Timelines from Indian History",
+author:"Anurima Chanda",
+price:"639Rs",
+rating:45
+
+},{
+img:"https://images-eu.ssl-images-amazon.com/images/I/51rLaN32HjS._AC_SX184_.jpg",
+title:"Funeral Nights",
+author:"Kynpham Sing Nongkynrih",
+price:"707Rs",
+rating:56
+}];
+
+const  Greeting = ()=>{
+
+    return( 
+        <>
+        <article className="booklist">
+        
+              {
+                BookList.map(b =>
+                  <Books book={b}></Books> 
+                )
+              }
+       
+         
+         </article>
+         </>
+         );
+
+   
+}
+
+const Books = (props)=>{
+    
+    return (
+    
+        <article className="book">
+         <img src={props.book.img} />
+         <h3>{props.book.title}</h3>
+         <h4>{props.book.author}</h4>
+         <h4 id="price">{props.book.price}</h4>
+         <Rating className="rating" no_rat = {props.book.rating}></Rating>
+         <button type="button">Buy Now</button>
+         <button type="button" id="add_cart">Add</button>
+        </article>
+    
+    );
+}
+
+
+const Rating = ({no_rat}) => {
+    console.log(no_rat)
+return (
+<>
+<div className="rating_div">
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star unchecked"></span>
+<span class="fa fa-star unchecked"></span>
+<p>({no_rat})</p>
+</div>
+</>
 );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<Greeting/>, document.getElementById("root"));
